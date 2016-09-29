bc36a1a9de7f5c5d10793d8eb0ad212059411173
Chaimaebettah <YOUR EMAIL ADDRESS>
2016-09-29 07:25:31 -0700

    form

--- app.js
+++ app.js
@@ -2,8 +2,35 @@
 var hours = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 //create a constraction function
 var selectCookies = document.getElementById('cookieslocation');
-var myLocations = [];
-function Location (locationName, minCustPerHour, maxCustPerHour, avgCookiesPerCust) {
+var myLocations = [
+  new CookieStand('firstAndPike',23, 65, 6.3),
+  new CookieStand('SeaTacAirport', 3, 24, 1.2),
+  new CookieStand('SeattleCenter',11 ,38,3.7),
+  new CookieStand('CapitolHill',20 ,38 ,2.3),
+  new CookieStand('Alki',2,16,4.6),
+];
+var locationForm = document.getElementById('cookieform');
+
+//creates a tr and appends a child to it.
+function row(child){
+  var element = document.createElement('tr');
+  if(child){
+    element.appendChild(child);
+  }
+  return element;
+}
+
+//creates a th or td and appends text to it.
+function column(child, type){
+  var element = document.createElement(type);
+  if(child){
+    element.textContent = child;
+  }
+  return element;
+}
+
+
+function CookieStand (locationName, minCustPerHour, maxCustPerHour, avgCookiesPerCust) {
   this.locationName = locationName;
   this.minCustPerHour = minCustPerHour;
   this.maxCustPerHour = maxCustPerHour;
@@ -25,98 +52,67 @@ function Location (locationName, minCustPerHour, maxCustPerHour, avgCookiesPerCu
   };
   this.render = function(){
     this.cookiesPurchasedPerHour();
-
-    //render the locations name on the table
-    var trEl = document.createElement('tr');
-    var firstColumn = document.createElement('td');
-    firstColumn.textContent = this.locationName;
-    trEl.appendChild(firstColumn);
-
-//render the total cookies per hour for each location.
-    for(var i = 0; i < this.totalCookiesperHour.length; i++){
-      var tdEl = document.createElement('td');
-      tdEl.textContent = this.totalCookiesperHour[i];
-      trEl.appendChild(tdEl);
+    var rowElement = row(column(this.locationName,'td'));
+    //render the total cookies per hour for each location.
+    for(var i = 0; i < hours.length; i++){
+      rowElement.appendChild(column(this.totalCookiesperHour[i],'td'));
     }
-    selectCookies.appendChild(trEl);
-//render the daily location total for each location.
-    var tdTotal = document.createElement('td');
-    tdTotal.textContent = this.totalDailyCookieSale;
-    trEl.appendChild(tdTotal);
-
+    rowElement.appendChild(column(this.totalDailyCookieSale,'td'));
+    selectCookies.appendChild(rowElement);
   };
-  this.render();
-  myLocations.push(this);
-
 };
 
 //render the empty th
 function displayTableHeader() {
-  var trEl = document.createElement('tr');
-  var emptyTrEl = document.createElement('th');
-  emptyTrEl.textContent = '';
-  trEl.appendChild(emptyTrEl);
+  var rowElement = row(column('Locations','th'));
   //render the hours
   for(var i = 0; i < hours.length; i++){
-    var thEl = document.createElement('th');
-    thEl.textContent = hours[i];
-    trEl.appendChild(thEl);
+    rowElement.appendChild(column(hours[i],'th'));
   }
-
   //render the  column daily location total
-  var lastColumn = document.createElement('td');
-  lastColumn.textContent = 'Daily location total';
-  trEl.appendChild(lastColumn);
-  selectCookies.appendChild(trEl);
-
+  rowElement.appendChild(column('Location total','th'));
+  selectCookies.appendChild(rowElement);
 }
-
 function displayTableFooter() {
-  var trEl = document.createElement('tr');
-  var totalEl = document.createElement('th');
-  totalEl.textContent = 'Total';
-  trEl.appendChild(totalEl);
-  var totalOfTotal = 0;
+  var rowElement = row(column('Total','th'));
+  var totalOfDailyCookies = 0;
   for( var a = 0; a < hours.length; a++){
-    var totalCookiesPerHour = 0;
-
+    var total = 0;
     for(var b = 0; b < myLocations.length; b++){
-        // console.log(myLocations[b].totalCookiesperHour)
-      for( var c = 0; c < myLocations[b].totalCookiesperHour.length; c++){
-        if(a === c){
-          totalCookiesPerHour += myLocations[b].totalCookiesperHour[a];
-          totalOfTotal = totalCookiesPerHour;
-        }
-      }
+      total += myLocations[b].totalCookiesperHour[a];
     }
-    console.log(totalCookiesPerHour);
-    var totalForEachhour = document.createElement('th');
-    totalForEachhour.textContent = totalOfTotal;
-    trEl.appendChild(totalForEachhour);
-
-
-
+    rowElement.appendChild(column(total,'th'));
+    totalOfDailyCookies += total;
   }
-  var totals = document.createElement('th');
-  totals.textContent = totalOfTotal;
-  trEl.appendChild(totals);
-
-  selectCookies.appendChild(trEl);
-
+  rowElement.appendChild(column(totalOfDailyCookies,'th'));
+  selectCookies.appendChild(rowElement);
 }
+//loop through all locations and render it.
+function displayLocations() {
+  for(var i = 0; i < myLocations.length; i++) {
+    myLocations[i].render();
+  }
+}
+//displays the table.
+function displayTable(){
+  //clear the table before creating elements
+  selectCookies.textContent = '';
+  displayTableHeader();
+  displayLocations();
+  displayTableFooter();
+}
+displayTable();
 
+//get the values from the form and render it.
+locationForm.addEventListener('submit',function(event){
+  event.preventDefault();
+  var nameInput = event.target.name.value;
+  var minInput = parseInt(event.target.min.value);
+  var maxInput = parseInt(event.target.max.value);
+  var avgInput = parseInt(event.target.avg.value);
+  var newLocation = new CookieStand(nameInput,minInput,maxInput,avgInput);
+  myLocations.unshift(newLocation);
+  console.log(newLocation);
+  displayTable();
 
-displayTableHeader();
-// create a 5 instance.
-new Location('firstAndPike',23, 65, 6.3);
-new Location('SeaTacAirport', 3, 24, 1.2);
-new Location('SeattleCenter',11 ,38,3.7);
-new Location('CapitolHill',20 ,38 ,2.3);
-new Location('Alki',2,16,4.6);
-
-displayTableFooter();
-
-
-
-// console.log(myLocations);
-// console.log(myLocations.totalDailyCookieSale);
+});
